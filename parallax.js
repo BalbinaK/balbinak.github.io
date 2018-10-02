36b33517af553a1852d327852e2c7f3f993f74e4
Balbuhhha <balbuhhha@gmail.com>
2018-10-02 14:50:47 +0200

    fixed a few bugs in header

--- parallax.js
+++ parallax.js
@@ -1,2 +1,10 @@
 var headerImage = document.querySelector('.parallax background-01')
+console.log(headerImage)
 var footerImage = document.querySelector('background-01-bottom')
+
+window.addEventListener('scroll', function() {
+  var screenOffset = window.scrollY;
+  var headerImageOffset = screenOffset * 0.5;
+  headerImage.style.top = 0 + headerImageOffset + "px";
+
+})
