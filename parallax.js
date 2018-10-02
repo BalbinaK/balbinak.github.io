d2545248c417aa03b09e8a0261c28e30fd53939d
Balbuhhha <balbuhhha@gmail.com>
2018-10-02 16:21:34 +0200

    added some more parallax effect in header

--- parallax.js
+++ parallax.js
@@ -5,13 +5,13 @@ var footerImage = document.querySelector('.background-01-bottom')
 
 window.addEventListener('scroll', function() {
   var screenOffset = window.scrollY;
-  var headerImageOffset = screenOffset * 0.3;
+  var headerImageOffset = screenOffset * 0.7;
   var footerImageOffset = screenOffset * 0.5;
-  headerImage.style.top = headerImageOffset + 'px';
+  headerImage.style.height = 800 - headerImageOffset + 'px';
   if (window.innerWidth >= 700) {
-    footerImage.style.height = -500 + footerImageOffset + 'px';
+    footerImage.style.height = -200 + footerImageOffset + 'px';
   }
   if (window.innerWidth >= 1200) {
-    footerImage.style.height = -350 + footerImageOffset + 'px';
+    footerImage.style.height = 0 + footerImageOffset + 'px';
   }
 })
