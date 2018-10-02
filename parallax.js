13efbcb4c67590074516ef680c5cc1fabdbd6a10
Balbuhhha <balbuhhha@gmail.com>
2018-10-02 15:24:53 +0200

    parallax of header and footer background completed

--- parallax.js
+++ parallax.js
@@ -1,10 +1,17 @@
-var headerImage = document.querySelector('.parallax background-01')
-console.log(headerImage)
-var footerImage = document.querySelector('background-01-bottom')
+var headerImage = document.querySelector('.background-01')
+var footerImage = document.querySelector('.background-01-bottom')
+
+
 
 window.addEventListener('scroll', function() {
   var screenOffset = window.scrollY;
-  var headerImageOffset = screenOffset * 0.5;
-  headerImage.style.top = 0 + headerImageOffset + "px";
-
+  var headerImageOffset = screenOffset * 0.3;
+  var footerImageOffset = screenOffset * 0.5;
+  headerImage.style.top = headerImageOffset + 'px';
+  if (window.innerWidth >= 700) {
+    footerImage.style.height = -500 + footerImageOffset + 'px';
+  }
+  if (window.innerWidth >= 1200) {
+    footerImage.style.height = -350 + footerImageOffset + 'px';
+  }
 })
