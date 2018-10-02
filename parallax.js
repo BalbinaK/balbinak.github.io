51affa712162cc2276e2719d939c406349b0683c
Balbuhhha <balbuhhha@gmail.com>
2018-10-02 17:18:13 +0200

    small screen about me without parallax but with opacity

--- parallax.js
+++ parallax.js
@@ -9,11 +9,14 @@ window.addEventListener('scroll', function() {
   var footerImageOffset = screenOffset * 0.5;
   var aboutMeImageOffset = screenOffset * 0.5;
   headerImage.style.height = 800 - headerImageOffset + 'px';
+  // aboutMeImage.style.height = 0 + aboutMeImageOffset + 'px';
   if (window.innerWidth >= 700) {
     footerImage.style.height = -200 + footerImageOffset + 'px';
-    aboutMeImage.style.height = 200 + aboutMeImageOffset + 'px';
+    aboutMeImage.style.height = 300 + aboutMeImageOffset + 'px';
   }
   if (window.innerWidth >= 1200) {
     footerImage.style.height = 0 + footerImageOffset + 'px';
+    aboutMeImage.style.height = 700 + aboutMeImageOffset + 'px';
   }
+  
 })
