1fa9e3c158acef733c4608ece80bbaf19b9f89c1
Balbuhhha <balbuhhha@gmail.com>
2018-10-04 11:35:50 +0200

    scroll-spy works

--- carousel.js
+++ carousel.js
@@ -1,38 +1,40 @@
-var previousButton = document.querySelector('.interests__previous-button');
-var nextButton = document.querySelector('.interests__next-button');
-var navigation = document.querySelector('.interests__navigation');
+(function () {
+  var previousButton = document.querySelector('.interests__previous-button');
+  var nextButton = document.querySelector('.interests__next-button');
+  var navigation = document.querySelector('.interests__navigation');
 
-nextButton.addEventListener('click', function () {
-  clearInterval(intervalId);
-  activateNextSlide()
-});
-previousButton.addEventListener('click', function () {
-  clearInterval(intervalId);
-  activatePreviousSlide()
-});
+  nextButton.addEventListener('click', function () {
+    clearInterval(intervalId);
+    activateNextSlide()
+  });
+  previousButton.addEventListener('click', function () {
+    clearInterval(intervalId);
+    activatePreviousSlide()
+  });
 
-var intervalId = setInterval(function () {
-  activateNextSlide()
-}, 2000);
+  var intervalId = setInterval(function () {
+    activateNextSlide()
+  }, 2000);
 
-function activateNextSlide() {
-  var element = document.querySelector('.interests-items img:first-child');
-  element.parentElement.appendChild(element);
+  function activateNextSlide() {
+    var element = document.querySelector('.interests-items img:first-child');
+    element.parentElement.appendChild(element);
 
-  var current = navigation.querySelector('.active');
-  var next = current.nextElementSibling || navigation.querySelector('span');
+    var current = navigation.querySelector('.active');
+    var next = current.nextElementSibling || navigation.querySelector('span');
 
-  current.classList.remove('active');
-  next.classList.add('active');
-}
+    current.classList.remove('active');
+    next.classList.add('active');
+  }
 
-function activatePreviousSlide() {
-  var element = document.querySelector('.interests-items img:last-child');
-  element.parentElement.prepend(element);
+  function activatePreviousSlide() {
+    var element = document.querySelector('.interests-items img:last-child');
+    element.parentElement.prepend(element);
 
-  var current = navigation.querySelector('.active');
-  var previous = current.previousElementSibling || navigation.querySelector('span:last-child');
+    var current = navigation.querySelector('.active');
+    var previous = current.previousElementSibling || navigation.querySelector('span:last-child');
 
-  current.classList.remove('active');
-  previous.classList.add('active');
-}
+    current.classList.remove('active');
+    previous.classList.add('active');
+  }
+})()
--- parallax.js
+++ parallax.js
@@ -1,22 +1,25 @@
-var headerImage = document.querySelector('.background-01');
-var footerImage = document.querySelector('.background-01-bottom');
-var aboutMeImage = document.querySelector('.background-about-me');
+(function () {
+
+  var headerImage = document.querySelector('.background-01');
+  var footerImage = document.querySelector('.background-01-bottom');
+  var aboutMeImage = document.querySelector('.background-about-me');
 
 
-window.addEventListener('scroll', function() {
-  var screenOffset = window.scrollY;
-  var headerImageOffset = screenOffset * 0.7;
-  var footerImageOffset = screenOffset * 0.5;
-  var aboutMeImageOffset = screenOffset * 0.5;
-  headerImage.style.height = 800 - headerImageOffset + 'px';
-  aboutMeImage.style.height = 0 + aboutMeImageOffset + 'px';
-  if (window.innerWidth >= 700) {
-    footerImage.style.height = -400 + footerImageOffset + 'px';
-    aboutMeImage.style.height = 300 + aboutMeImageOffset + 'px';
-  }
-  if (window.innerWidth >= 1200) {
-    footerImage.style.height = -250 + footerImageOffset + 'px';
-    aboutMeImage.style.height = 700 + aboutMeImageOffset + 'px';
-  }
-  
-})
+  window.addEventListener('scroll', function () {
+    var screenOffset = window.scrollY;
+    var headerImageOffset = screenOffset * 0.7;
+    var footerImageOffset = screenOffset * 0.5;
+    var aboutMeImageOffset = screenOffset * 0.5;
+    headerImage.style.height = 800 - headerImageOffset + 'px';
+    aboutMeImage.style.height = 0 + aboutMeImageOffset + 'px';
+    if (window.innerWidth >= 700) {
+      footerImage.style.height = -400 + footerImageOffset + 'px';
+      aboutMeImage.style.height = 300 + aboutMeImageOffset + 'px';
+    }
+    if (window.innerWidth >= 1200) {
+      footerImage.style.height = -250 + footerImageOffset + 'px';
+      aboutMeImage.style.height = 700 + aboutMeImageOffset + 'px';
+    }
+
+  })
+})()
