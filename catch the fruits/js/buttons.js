65376f6fa883567d129bf1fecadb1a95a3f83401
Balbuhhha <balbuhhha@gmail.com>
2018-11-18 10:31:24 +0100

    eventListener for mobiles and desktop finished

--- catch the fruits/js/buttons.js
+++ catch the fruits/js/buttons.js
@@ -34,6 +34,7 @@ function resetScoreAndLife() {
 introScreenAppears()
 
 function handleButtonClick(event){
+    event.preventDefault()
     if (event.target.classList.contains('button')) {
         var introScreen = document.querySelector('.introScreen');
         var endScreen = document.querySelector('.endScreen');
